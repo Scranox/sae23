e3d946a12e75b6d386fe4c4f166fe8f567c98fb7
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2024-06-04 15:19:34 +0200

    add administration pages

--- loginAdministration.php
+++ loginAdministration.php
@@ -1,14 +1,19 @@
-<form action="loginAdministrationProcess.php" method="post">
-  <div class="container">
-    <label for="username"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required>
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="loginAdministrationBack.php" method="post">
+        <div class="container">
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
 
-    <label for="password"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required>
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
 
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-</form> 
+            <button type="submit">Login</button>
+            <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+        </form> 
+    <body>
+</html>
