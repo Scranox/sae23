20ffd43bdaf9fcae68bc2da32124e299cde12032
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2024-06-04 15:48:20 +0200

    login back end

--- loginAdministrationBack.php
+++ loginAdministrationBack.php
@@ -2,14 +2,37 @@
 
 
 session_start();
-$_SESSION["password"]=$_REQUEST["password"];  // Récupération du mot de passe
-$motdepasse=$_SESSION["password"];
+$_SESSION["password"]=$_REQUEST["password"];
+$password=$_SESSION["password"];
+$_SESSION["username"]=$_REQUEST["username"]; 
+$username=$_SESSION["username"];
 $_SESSION["auth"]=FALSE;
 
 if (!empty($motdepasse)){
-    echo "cool";
+    include ("mysql.php");
+
+	$requete = "SELECT `login`, `mdp` FROM `administration`";
+	$resultat = mysqli_query($id_bd, $requete)
+		or die("Execution de la requete impossible : $requete");
+
+	$ligne = mysqli_fetch_row($resultat);
+	if ($username==$ligne[0])
+	{
+        if ($password==$ligne[1]){
+            $_SESSION["auth"]=TRUE;		
+            mysqli_close($id_bd);
+		    echo "<script type='text/javascript'>document.location.replace('choix_type.php');</script>";
+        }	
+	}
+	else
+	{
+		$_SESSION = array();
+        session_destroy();
+        unset($_SESSION); 
+        mysqli_close($id_bd);
+    }
 } else {
-    echo "nein";
+    echo "<script type='text/javascript'>document.location.replace('login_error.php');</script>";
 }
 
 ?>
