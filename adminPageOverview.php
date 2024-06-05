ea6cdcba3d480634b7426e2107641b64de9acb8f
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2024-06-05 17:28:49 +0200

    Fix admin page overview

--- adminPageOverview.php
+++ adminPageOverview.php
@@ -1,13 +1,11 @@
 <?php 
 
-    
-
     session_start();
     if(isset($_SESSION["authenticated"]) && $_SESSION["authenticated"]==FALSE){
-        $username=$_SESSION["username"]
-        header("Location: ./loginAdminProcessing.php")
+        header("Location: ./loginFormAdmin.php")
     }
 
+    $username=$_SESSION["username"]
     echo "$username"
 
 ?>
