740bb22d84b64cb422d77539fab900eaa8ae27e2
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2024-06-05 18:05:03 +0200

    final 05 06-24 adrien commit

--- adminPageOverview.php
+++ adminPageOverview.php
@@ -9,3 +9,30 @@
     }
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>SAÉ 23</title>
+    <link rel="stylesheet" type="text/css" href="./styles/main.css">
+</head>
+<body>
+    <header>
+        <br><hr><h1>SAÉ 23</h1><hr><br>
+    </header>
+    <div>
+        
+    </div>
+    <footer>
+        <hr>
+        <ul>
+            <li id="footerleft"><p><a href="./projet.php">Gestion de projet</a></p></li>
+            <li id="footercenter"><p><a href="./adminPageOverview.php">Accès administrateur</a></p></li>
+            <li id="footerright"><p><a href="./loginGestionnaire.html">Accès gestionnaire</a></p></li>
+        </ul>
+    </footer>
+
+    <script src="./scripts/fonts.js"></script>
+</body>
+</html>
+
